86bc9b305997954bb4a1db329dbd97dba4149683
fredy1617 <alfredomartinez6510@gmail.com>
2018-07-05 13:59:53 -0500

    routes y storange
    routes y storange

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('revista', 'RevistaController');
+Route::resource('infobase', 'BaseController');
+Route::resource('autor', 'AutorController');
+Route::resource('tema', 'TemaController');
+
+Route::resource('Form1', 'Form1Controller');
+Route::resource('Form2', 'Form2Controller');
+Route::resource('Form3', 'Form3Controller');
+Route::resource('Form4', 'Form4Controller');
+Route::resource('Form5', 'Form5Controller');
+Route::resource('Form6', 'Form6Controller');
+Route::resource('Form7', 'Form7Controller');
+
+Route::resource('categoria', 'CategoriaController');
+
+Route::resource('proveedor', 'ProveedorController');
+
+Route::resource('producto', 'ProductoController');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
